cdb6f07ee221a10b70195eb0b5ab49101a9d4776
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2023-04-04 22:58:25 +0200

    fix: export naming

--- server.lua
+++ server.lua
@@ -1,4 +1,4 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+local QBCore = exports['qbx-core']:GetCoreObject()
 
 QBCore.Functions.CreateCallback('qb-scoreboard:server:GetConfig', function(_, cb)
     cb(Config.IllegalActions)
